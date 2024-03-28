c8486074a136958204471089d642d6a2154f8af9
LAPTOP-I04HNM2I\kim <woong2209@gmail.com>
2024-03-28 14:02:43 +0900

    Revert "24.03.28(THU) 0.2.3.8"
    This reverts commit 9c0e5ce9f9561fdfb1a84dbbd6f7af05be1b1f13.

--- src/main/java/infinitystone/chalKag/controller/headHuntPost/HeadHuntPostSingleController.java
+++ src/main/java/infinitystone/chalKag/controller/headHuntPost/HeadHuntPostSingleController.java
@@ -2,8 +2,6 @@ package infinitystone.chalKag.controller.headHuntPost;
 
 import infinitystone.chalKag.biz.comment.CommentDTO;
 import infinitystone.chalKag.biz.comment.CommentService;
-import infinitystone.chalKag.biz.postImg.PostImgDTO;
-import infinitystone.chalKag.biz.postImg.PostImgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,28 +20,18 @@ public class HeadHuntPostSingleController {
 
 	@Autowired
 	private CommentService commentService;
-
-	@Autowired
-	private PostImgService postImgService;
 	
 	@RequestMapping("/headHuntPostSingle")
-	public String headHuntPostList(Model model, HeadHuntPostDTO headHuntPostDTO, CommentDTO commentDTO, PostImgDTO postImgDTO) {
+	public String headHuntPostList(Model model, HeadHuntPostDTO headHuntPostDTO, CommentDTO commentDTO) {
 
 		// headHuntPostDTO에 있는 정보로 게시글 내용 불러오기
-		headHuntPostDTO.setSearchCondition("headHuntPostSingle");
+
 		headHuntPostDTO = headHuntPostService.selectOne(headHuntPostDTO);
 		commentDTO.setPostId(headHuntPostDTO.getHeadHuntPostId());
 
-		postImgDTO.setPostId(headHuntPostDTO.getHeadHuntPostId());
-		postImgDTO.setSearchCondition("headHuntPostSingleImg");
-
-		List<PostImgDTO> postImgList = postImgService.selectAll(postImgDTO);
-		List<CommentDTO> commentList = commentService.selectAll(commentDTO);
-
-
+		List<CommentDTO> commentDatas = commentService.selectAll(commentDTO);
 		model.addAttribute("headHuntPostSingle",headHuntPostDTO);
-		model.addAttribute("commentList", commentList);
-		model.addAttribute("postImgList", postImgList);
+		model.addAttribute("commentList", commentDatas);
 
 		return "headHuntPostSingle";
 	}
